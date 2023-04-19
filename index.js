1b9615ed348d5888c47983d617d11ccf58542d8d
Shajib-Dv <mdsojeeb242@gmail.com>
2023-04-19 14:15:41 +0600

    filter data by id

--- index.js
+++ index.js
@@ -20,6 +20,12 @@ app.get("/news", (req, res) => {
   res.send(news);
 });
 
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const singleNews = news.filter((n) => n["_id"] === id) || {};
+  res.send(singleNews);
+});
+
 app.listen(port, () => {
   console.log(`Port running on ${port}`);
 });
