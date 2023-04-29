3b195b936b67b63c1ed2630a6b0713db4e8e85ae
Shajib-Dv <mdsojeeb242@gmail.com>
2023-04-29 19:44:31 +0600

    .env added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 const catagories = require("./data/categories.json");
 const news = require("./data/news.json");
